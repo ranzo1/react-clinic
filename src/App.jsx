85c7ab62a1a9d17e5d8e86b87cbad6694590b315
Zoran Protic <zoran.protic@payspot.co.rs>
2022-05-17 16:07:06 +0200

    main page fondation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Introduction from './components/Introduction/Introduction'
+import OurServices from './components/ourServices/OurServices'
+import About from './components/about/About'
+import OurDoctors from './components/ourDoctors/OurDoctors'
+import BookNow from './components/bookNow/BookNow'
+import ClientsReview from './components/clientsReview/ClientsReview'
+import Footer from './components/footer/Footer'
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div>
+      <Header />
+      <Introduction />
+      <OurServices />
+      <About />
+      <OurDoctors />
+      <BookNow />
+      <ClientsReview />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
